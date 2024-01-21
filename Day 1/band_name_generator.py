3f1431e0a60eeee6cf0d144fbef63b28300c1e0f
Afzal Koukar <afzalkoukar@gmail.com>
2024-01-22 00:34:58 +0530

    exercise 1.4

--- Day 1/band_name_generator.py
+++ Day 1/band_name_generator.py
@@ -1,4 +1,4 @@
-print("Welcome to Band Name Generator")
-place = input("Enter the city you grew up in: ")
-pet = input("Enter the name of your pet: ")
+print("Welcome to Band Name Generator: \n")
+place = input("Enter the city you grew up in: \n")
+pet = input("Enter the name of your pet: \n")
 print("Your band name is", place, pet)
---
+++ Day 1/exercise-1-4.py
@@ -0,0 +1,8 @@
+a = input("Input a: ")
+b = input("Input b: ")
+c=a
+a=b
+b=c
+print("A is now: " + a)
+print("B is now:  " + b)
+
